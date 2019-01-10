3b33e2ce71473e4306d8f5141676c9173fc31cfc
Reka <rekapap28@gmail.com>
2019-01-10 17:50:35 +0000

    adds release bombs functionality and iterating the game

--- src/game.js
+++ src/game.js
@@ -132,9 +132,27 @@ function update ()
 
 function collectStar (player, star){
   star.disableBody(true, true);
-
   score += 10;
   scoreText.setText('Score: ' + score);
+
+  if (stars.countActive(true) === 0)
+  {
+      // reenabling stars
+      stars.children.iterate(function (child) {
+
+          child.enableBody(true, child.x, 0, true, true);
+
+      });
+      // random number for Bombs' position
+      var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+      // creating bombs
+      var bomb = bombs.create(x, 16, 'bomb');
+      bomb.setBounce(1);
+      bomb.setCollideWorldBounds(true);
+      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+      bomb.allowGravity = false;
+  }
 }
 
 function hitBomb (player, bomb) {
